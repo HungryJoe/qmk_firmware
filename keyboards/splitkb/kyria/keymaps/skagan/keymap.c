dd50074ae524f11fb9b694f8d23457617170b934
Sam Kagan <skagan@nrao.edu>
2023-07-28 17:44:23 -0600

    Fixed lack of enter key

--- keyboards/splitkb/kyria/keymaps/skagan/keymap.c
+++ keyboards/splitkb/kyria/keymaps/skagan/keymap.c
@@ -67,15 +67,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * | `~     |   Z  |   X  |   C  |   V  |   B  |      | FKEYS|  |      |      |   N  |   M  | ,  < | . >  | /  ? | =+     |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        | LGUI | LCtrl| SYM  |LShift| Space|  | Space|Rshift| Bksp | RCtrl| RAlt |
+ *                        | LGUI | LCtrl| SYM  |LShift| Space|  | Enter|Rshift| Bksp | RCtrl| RAlt |
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_SAM] = LAYOUT(
      KC_ESC  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y ,  KC_U ,  KC_I ,   KC_O ,  KC_P , KC_MINS,
      KC_TAB , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H  ,  KC_J ,  KC_K ,   KC_L ,KC_SCLN, KC_QUOT,
-     KC_GRV , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B ,_______,TT(_FUNCTION),     _______, _______, KC_N  ,  KC_M , KC_COMM, KC_DOT ,KC_SLSH, KC_EQL,
-                                KC_LGUI, KC_LCTL, SYM ,  KC_LSFT, KC_SPC,     KC_SPC ,KC_RSFT ,KC_BSPC,KC_RCTL, KC_RALT
+     KC_GRV , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B ,_______,TT(_FUNCTION),_______, _______, KC_N  ,  KC_M , KC_COMM, KC_DOT ,KC_SLSH, KC_EQL,
+                                KC_LGUI, KC_LCTL, SYM ,  KC_LSFT, KC_SPC,       KC_ENT,KC_RSFT ,KC_BSPC,KC_RCTL, KC_RALT
     ),
 
 /*
