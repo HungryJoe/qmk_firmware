548686b0799caa8918d082999d6bf0e2cd5512ce
Sam Kagan <skagan@nrao.edu>
2023-08-08 17:53:01 -0600

    Simplified symbol layer

--- keyboards/splitkb/kyria/keymaps/skagan/keymap.c
+++ keyboards/splitkb/kyria/keymaps/skagan/keymap.c
@@ -52,20 +52,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Base Layer: Sam
  *
  * ,-------------------------------------------.                              ,-------------------------------------------.
- * |  Esc   |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |  -_    |
+ * |  {[    |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |  -_    |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * | Tab    |   A  |   S  |   D  |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : | '"     |
+ * | Esc    |   A  |   S  |   D  |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : | '"     |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * | `~     |   Z  |   X  |   C  |   V  |   B  | LAlt | LCtrl|  | RCtrl| RAlt |   N  |   M  | ,  < | . >  | /  ? | =+     |
+ * | ]}     |   Z  |   X  |   C  |   V  |   B  | LAlt | LCtrl|  | RCtrl| RAlt |   N  |   M  | ,  < | . >  | /  ? | =+     |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
  *                        | LGUI | FKEYS| SYM  |LShift| Space|  | Enter|Rshift| Bksp | Tab  | RGUI |
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_SAM] = LAYOUT(
-     KC_ESC , KC_Q ,  KC_W   ,  KC_E  , KC_R  ,KC_T ,                                        KC_Y  , KC_U ,  KC_I ,   KC_O ,  KC_P , KC_MINS,
-     KC_TAB , KC_A ,  KC_S   ,  KC_D  ,  KC_F ,KC_G ,                                        KC_H  , KC_J ,  KC_K ,   KC_L ,KC_SCLN, KC_QUOT,
-     KC_GRV , KC_Z ,  KC_X   ,  KC_C   ,  KC_V, KC_B,KC_LALT,KC_LCTL,      KC_RCTL,KC_RALT , KC_N  , KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_EQL ,
+     KC_LBRC, KC_Q ,  KC_W   ,  KC_E  , KC_R  ,KC_T ,                                        KC_Y  , KC_U ,  KC_I ,   KC_O ,  KC_P , KC_MINS,
+     KC_ESC , KC_A ,  KC_S   ,  KC_D  ,  KC_F ,KC_G ,                                        KC_H  , KC_J ,  KC_K ,   KC_L ,KC_SCLN, KC_QUOT,
+     KC_RBRC, KC_Z ,  KC_X   ,  KC_C   ,  KC_V, KC_B,KC_LALT,KC_LCTL,      KC_RCTL,KC_RALT , KC_N  , KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_EQL ,
                                 KC_LGUI, FKEYS, SYM ,KC_LSFT, KC_SPC,       KC_ENT,KC_RSFT ,KC_BSPC,KC_TAB,KC_RGUI
     ),
 
@@ -157,20 +157,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Sym Layer: Numbers and symbols
  *
  * ,-------------------------------------------.                              ,-------------------------------------------.
- * |   ~    |  1   |  2   |  3   |  4   |  5   |                              |   6  |  7   |  8   |  9   |  0   |        |
+ * |        |  1   |  2   |  3   |  4   |  5   |                              |   6  |  7   |  8   |  9   |  0   |        |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |   ?    |  !   |  @   |  #   |  $   |  %   |                              |   ^  |  &   |  *   |  (   |  )   |        |
+ * |        |  !   |  @   |  #   |  $   |  %   |                              |   ^  |  &   |  *   |  (   |  )   |        |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * |        |   |  |  \   |  [   |  {   |  <   |      |      |  |      |      |   >  |  }   |  ]   |  +   |  =   |        |
+ * |        |      |      |      |  `   |  ~   |      |      |  |      |      |   \  |  |   |      |      |      |        |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_SYM] = LAYOUT(
-      KC_TILD,   KC_1 ,   KC_2 ,   KC_3 ,   KC_4 ,   KC_5 ,                                       KC_6 ,   KC_7 ,   KC_8 ,   KC_9 ,   KC_0 , _______,
-      KC_QUES, KC_EXLM,  KC_AT , KC_HASH,  KC_DLR, KC_PERC,                                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
-      _______, KC_PIPE, KC_BSLS, KC_LBRC, KC_LCBR, KC_LT  , _______, _______, _______, _______, KC_GT  , KC_RCBR, KC_RBRC, KC_PLUS,  KC_EQL, _______,
+      _______,   KC_1 ,   KC_2 ,   KC_3 ,   KC_4 ,   KC_5 ,                                       KC_6 ,   KC_7 ,   KC_8 ,   KC_9 ,   KC_0 , _______,
+      _______, KC_EXLM,  KC_AT , KC_HASH,  KC_DLR, KC_PERC,                                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+      _______, _______, _______, _______,  KC_GRV, KC_TILD, _______, _______, _______, _______, KC_BSLS, KC_PIPE, _______, _______, _______, _______,
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
